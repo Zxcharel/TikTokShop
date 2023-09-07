86b58c9344b9508bceaf81dc9de7669fa3863673
Zxcharel <104544143+Zxcharel@users.noreply.github.com>
2023-09-07 22:42:10 +0800

    Update friend_cart.php

--- friend_cart.php
+++ friend_cart.php
@@ -54,7 +54,7 @@
     <body>
         <section class="bg_image_1">
         <div>
-            <img src="img/friend_cart.PNG" id="shop_pg">
+            <img src="img/friend_cart.png" id="shop_pg">
             <img src="img/phoneframe.png" alt="Phone Frame" id="phone-frame">
             <a href="friend.php">
                 <button type='button' class='prevpg btn btn-link'></button>
